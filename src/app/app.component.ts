8fcbf6b4a441528c8f54ac88e3d993b20da73159
Roberto Irigoyen <irigoyen.baisson@gmail.com>
2019-03-21 21:46:14 +0100

    Exercise pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { resolve } from 'url';
+import { reject } from 'q';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre = 'Roberto';
+
+
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  PI = Math.PI;
+
+  a:number =0.1234;
+
+  salario:number = 1234.5;
+
+  heroe ={
+    nombre:"Logan",
+    clave:"Wolverine",
+    edad:500,
+    direccion:{
+      calle:"Primera",
+      casa:"19"
+    }
+  };
+  valorDePromesa= new Promise( (resolve, reject) =>{
+    setTimeout( ()=> resolve('LLego la data'),3500);
+  });
+
+  fecha = new Date();
+
 }
